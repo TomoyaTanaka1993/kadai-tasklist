b738c9cceb74769637f37bd6cd5bf98e62249457
tomoya.tanaka <199tanaka@gmail.com>
2020-06-20 14:22:12 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,8 @@
 class TasksController < ApplicationController
+  
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy,:edit,:show]
+  
   def index
     @tasks = Task.all
   end
@@ -12,12 +16,13 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に作成されました'
       redirect_to @task
     else
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'Task が作成されませんでした'
       render :new
     end
@@ -40,16 +45,21 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
-
     flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+    redirect_back(fallback_location: root_path)
   end
   
     private
     
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,8 @@
-class ToppagesController < ApplicationController
+#class ToppagesController < ApplicationController
   def index
+    if logged_in?
+      @micropost = current_user.tasks.build  # form_with 用
+      @microposts = current_user.tasks.order(id: :desc).page(params[:page])
+    end
   end
-end
+#end
